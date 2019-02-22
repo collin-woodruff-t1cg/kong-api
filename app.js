9bc30aa45ad451571f72175faabab73631423319
Collin Woodruff <collin.woodruff@t1cg.com>
2019-02-22 14:31:40 -0500

    Added kong configurations

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // Require packages and set the port
 const express = require('express');
-const port = 3002;
+const port = 8045;
 const app = express();
  
-app.get('/public', (request, response) => {
+app.get('/api/non-auth', (request, response) => {
     response.send('Collin\'s Hello World Public');
 });
 
-app.get('/private', (request, response) => {
+app.get('/api/auth', (request, response) => {
     response.send('Collin\'s Hello World Private');
 });
  

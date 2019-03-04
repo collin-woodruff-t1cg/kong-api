f2fca7009e1dbddd415b8fa89d634d5bdb9122a9
collin-woodruff-t1cg <35041076+collin-woodruff-t1cg@users.noreply.github.com>
2019-03-04 14:19:20 -0500

    Update app.js

--- api/app.js
+++ api/app.js
@@ -4,11 +4,11 @@ const port = 8045;
 const app = express();
  
 app.use('/api/non-auth', (request, response) => {
-    response.send('Collin\'s Hello World Public!');
+    response.send('Collin\'s Hello World Public');
 });
 
 app.use('/api/auth', (request, response) => {
-    response.send('Collin\'s Hello World Private!');
+    response.send('Collin\'s Hello World Private');
 });
  
 // Start the server
